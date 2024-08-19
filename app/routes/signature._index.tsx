ae41f17acdaa0b27e20accb96777279e34ef23df
cmmerritt <christiane.merritt@gmail.com>
2024-08-18 18:47:03 -0700

    display random author under generated quote

--- app/routes/signature._index.tsx
+++ app/routes/signature._index.tsx
@@ -1,5 +1,6 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import authors from "shared/authors";
 
 export const loader = async () => {
   const quoteRes = await fetch(
@@ -18,11 +19,16 @@ export const loader = async () => {
 
 export default function Signature() {
   const { quoteRes } = useLoaderData<typeof loader>();
+  const randomAuthor = authors[Math.floor(Math.random()*authors.length)];
+  console.log(quoteRes)
   return (
     <main>
       <h1>Your New Email Signature</h1>
       <div>
         {quoteRes[0].quote}
+      </div>
+      <div>
+        ~{randomAuthor}
       </div>
     </main>
   );
